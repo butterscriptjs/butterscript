a3d238b82298697d3d9d64fc8e0ba9aeb9a3ea59
buhron <mbh90180@gmail.com>
2025-02-04 17:59:36 -0500

    just some stuff

--- butterscript.js
+++ butterscript.js
@@ -1,5 +1,5 @@
 // ButterScript v0.1.0
-const termutils = require('./butter_modules/termutils/index')
-termutils.write("ButterScript v0.1.0\n");
+const termutils = require('./lib/butter_modules/termutils/index')
+termutils.write("ButterScript v0.1.1\n");
 termutils.write("ButterScript is licensed with the GPL-2.0 license.\n");
 termutils.write(">>  ");
