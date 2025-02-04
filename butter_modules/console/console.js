25039922104aebe2f2b928c20f450d9c0850f08b
buhron <mbh90180@gmail.com>
2025-02-04 16:51:05 -0500

    some more progress

---
+++ butter_modules/console/console.js
@@ -0,0 +1,7 @@
+// very hard code i worked sleepless nights to write this code please send help 😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭
+function write(_a) {
+    process.stdout.write(_a)
+}
+function error(_err) {
+    process.stderr.write(_err)
+}
